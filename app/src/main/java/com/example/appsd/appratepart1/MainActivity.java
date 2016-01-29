f9b750bb94d4845c1017f3b2e28e6adf01c7a071
Md Bulbul Hossen <info247.bulbul@gmail.com>
2016-01-29 06:30:43 +0600

    Initial commit

---
+++ app/src/main/java/com/example/appsd/appratepart1/MainActivity.java
@@ -0,0 +1,46 @@
+package com.example.appsd.appratepart1;
+
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import hotchemi.android.rate.AppRate;
+import hotchemi.android.rate.OnClickButtonListener;
+
+
+public class MainActivity extends Activity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        AppRate.with(this)
+                //.setStoreType(AppRate.StoreType.GOOGLEPLAY) //default is Google, other option is Amazon
+                .setInstallDays(3) // default 10, 0 means install day.
+                .setLaunchTimes(10) // default 10 times.
+                .setRemindInterval(2) // default 1 day.
+                .setShowLaterButton(true) // default true.
+                .setDebug(true) // default false.
+                .setCancelable(false) // default false.
+                .setOnClickButtonListener(new OnClickButtonListener() { // callback listener.
+                    @Override
+                    public void onClickButton(int which) {
+                        Log.d(MainActivity.class.getName(), Integer.toString(which));
+                    }
+                })
+                .setMessage(R.string.meassage)
+                .setTitle(R.string.new_rate_dialog_title)
+                .setTextLater(R.string.new_rate_dialog_later)
+                .setTextNever(R.string.new_rate_dialog_never)
+                .setTextRateNow(R.string.new_rate_dialog_ok)
+                .monitor();
+
+
+        AppRate.showRateDialogIfMeetsConditions(this);
+    }
+
+
+}
